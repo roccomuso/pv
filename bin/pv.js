b1e0d13005961eb507d20e07f0103052af25103f
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2022-05-03 15:04:03 -0500

    Add -f option to format the ETA into days, hours, etc.

--- bin/pv.js
+++ bin/pv.js
@@ -4,6 +4,7 @@ var PV = require('../index')
 var bytes = require('bytes')
 var argv = require('minimist')(process.argv.slice(2), {alias: {
   'size': 's',
+  'format': 'f',
   'name': 'N',
   'version': 'v'
 }})
@@ -24,7 +25,7 @@ pv.on('info', function (info) {
   var percentage = info.percentage + '%'
   var speed = info.speed + '/s'
   var transferred = info.transferred + ' Transferred'
-  var eta = info.eta + ' ETA'
+  var eta = (argv.format ? formatEta(info.eta) : info.eta) + ' ETA'
   stderr(`${name} ${percentage} | ${eta} | ${transferred} | ${speed}`)
 })
 
@@ -35,4 +36,21 @@ function stderr (str) {
   process.stderr.clearLine() // clear current text
   process.stderr.cursorTo(0) // move cursor to beginning of line
   process.stderr.write(str)
+}
+
+function formatEta (eta) {
+  var days = Math.floor(eta / (60 * 60 * 24))
+  var hours = Math.floor(eta / (60 * 60)) % 24
+  var minutes = Math.floor(eta / 60) % 60
+  var seconds = Math.floor(eta) % 60
+
+  if (days) {
+    return days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's'
+  } else if (hours) {
+    return hours + 'h ' + minutes + 'm ' + seconds + 's'
+  } else if (minutes) {
+    return minutes + 'm ' + seconds + 's'
+  } else {
+    return seconds + 's'
+  }
 }
